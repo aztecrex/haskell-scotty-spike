44e74f5dedaec6a426ee50b2c23abb2438879cb8
Greg Wiley <aztec.rex@jammm.com>
2015-09-07 20:00:05 -0700

    environment selector

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,18 @@ This module is about configuring and launching a web server.  It delegates
 wiring resources to the Resources module.
  -}
 import Web.Scotty(scotty)
+import System.Environment(lookupEnv)
 
 import Resources
 
 main :: IO ()
 main = scotty 8099 resources
+
+
+data Environment = DEVELOPMENT | PRODUCTION
+env :: IO Environment
+env = do
+  envtoken <- lookupEnv "appenv"
+  return $ select envtoken where
+    select (Just "production") = PRODUCTION
+    select _                   = DEVELOPMENT
