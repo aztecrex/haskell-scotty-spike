a9938d5dbcddfd551bc5db6181228d89af660d11
Greg Wiley <aztec.rex@jammm.com>
2015-09-07 17:25:33 -0700

    more compact and readable resource assembly

--- app/Resources.hs
+++ app/Resources.hs
@@ -5,19 +5,15 @@ module Resources (resources) where
 import Web.Scotty (ScottyM, ActionM, get, text)
 import Control.Monad.IO.Class (liftIO)
 import Data.Text.Lazy (pack)
-import qualified Hello
-import qualified Answer
+import Hello
+import Answer
 
 action :: IO String -> ActionM()
 action r = do
   a <- liftIO r
   text $ pack a
 
-hello :: ScottyM()
-hello = get "/hello" $ action Hello.hello
-
-answer :: ScottyM()
-answer = get "/answer" $ action $ fmap show Answer.answer
-
 resources :: ScottyM()
-resources = hello >> answer
+resources = do
+  get "/hello" $ action Hello.hello
+  get "/answer" $ action $ fmap show Answer.answer
